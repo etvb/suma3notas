ae3e74b0c3e43f5f900f8fd22c6429a7a1f6afeb
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-08-19 08:54:08 -0500

    agregamos el valor de 2 notas mas, de forma aleatoria, que seran de 0 a 10

--- sumatresnotas/src/principal/Suma.java
+++ sumatresnotas/src/principal/Suma.java
@@ -1,22 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package principal;
 
 import java.util.Scanner;
 
-/**
- *
- * @author edgardo
- */
+
 public class Suma {
     public static void main(String[] args){
         
         Scanner entrada = new Scanner(System.in);
         
-        float nota1, nota2, nota3, suma;
+        float nota1, nota2, nota3, nota4, nota5, suma;
         
         System.out.println("Ingresa 3 calificaciones");
         
@@ -26,7 +19,12 @@ public class Suma {
         nota2 = entrada.nextFloat();
         System.out.print("Nota3: ");
         nota3 = entrada.nextFloat();
-        suma = nota1 + nota2 + nota3;
+        nota4 = (float) Math.random() * 10;
+        nota5 =(float) Math.random() * 10;
+        System.out.println(nota4);
+        System.out.println(nota5);
+
+        suma = nota1 + nota2 + nota3 + nota4;
         
         System.out.println("La suma es: " + suma);
         
